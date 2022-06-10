95a6baebf4bf56dc359c679dab13cdbaf1d7af8a
Sebastian <sebastiancurtislavarias@gmail.com>
2022-06-10 16:35:35 +0800

    save progress today.

---
+++ src/components/bloomberg/LatestNews.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: 'bloomberg-latest-news',
+
+    setup() {
+        const latestNews = ref([
+            {
+                category: 'Pursuits',
+                title: 'Japan Eases Foreign Tourism Ban, Allows Guided Package Tours',
+                time: 'updated 3h ago',
+            },
+            {
+                category: 'Green',
+                title: 'Ex-Nuclear Watchdog Chief Urges Japan to Win Atomic Support',
+                time: '3h ago',
+            },
+            {
+                category: 'Politics',
+                title: 'Japan’s Kishida Pressured Over Still-Strict Border Measures',
+                time: 'updated 4h ago',
+            },
+        ]);
+
+        return {
+            latestNews,
+        };
+    },
+});
+</script>
+
+<template>
+    <div class="space-y-2">
+        <div class="border-y border-black py-1">
+            <span class="text-2xl font-weight-medium text-red-500">LATEST</span>
+        </div>
+        <div class="space-y-3 divide-y divide-dashed divide-black">
+            <template v-for="(news, index) in latestNews" :key="index">
+                <div class="flex flex-col space-y-1 pt-2">
+                    <a href="#" class="text-sm hover:underline">{{
+                        news.category
+                    }}</a>
+                    <a href="#" class="text-md font-bold hover:underline">
+                        {{ news.title }}
+                    </a>
+                    <span class="text-xs text-gray-500">{{ news.time }}</span>
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
--- src/views/Bloomberg.vue
+++ src/views/Bloomberg.vue
@@ -5,9 +5,11 @@ import BloombergAssetStatusBar from '../components/bloomberg/AssetStatusBar.vue'
 import BloombergHeader from '../components/bloomberg/Header.vue';
 import BloombergNavBar from '../components/bloomberg/NavBar.vue';
 import BloombergHeadline from '../components/bloomberg/Headline.vue';
+import BloombergLatestNews from '../components/bloomberg/LatestNews.vue';
 
 export default defineComponent({
     components: {
+        BloombergLatestNews,
         BloombergHeadline,
         BloombergNavBar,
         BloombergHeader,
@@ -28,9 +30,18 @@ export default defineComponent({
             <bloomberg-asset-status-bar></bloomberg-asset-status-bar>
             <bloomberg-header></bloomberg-header>
             <bloomberg-nav-bar></bloomberg-nav-bar>
-            <div class="px-3">
+            <div class="px-3 space-y-5">
                 <bloomberg-headline></bloomberg-headline>
+                <div class="flex flex-row justify-start">
+                    <div class="w-1/6">
+                        <bloomberg-latest-news></bloomberg-latest-news>
+                    </div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </section>
+
+        <footer class="mt-20"></footer>
     </main>
 </template>
